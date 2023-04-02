755f7c6b39baf9d46250c6fdf746970aa456b9d8
bluebird8800000 <siusunshine03@gmail.com>
2023-04-02 16:26:52 +0900

    pacman

--- main.py
+++ main.py
@@ -1,60 +1,71 @@
-#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
+import pygame
+pygame.init()
 
-import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout, QLabel, QPushButton, QTextEdit, QVBoxLayout)
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPainter, QPen
+screen = pygame.display.set_mode([640, 640])
 
+WORLD_SIZE = 20
+BLOCK_SIZE = 32
+WIDTH = WORLD_SIZE*BLOCK_SIZE
+HEIGHT = WORLD_SIZE*BLOCK_SIZE
 
-class MyApp(QWidget):
+world = []
+pacman = pygame.image.load('images/pacman.png')
 
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-
-    def initUI(self):
-        pixmap = QPixmap('black.jpg')
-
-        lbl_img = QLabel()
-        lbl_img.setPixmap(pixmap)
-        lbl_size = QLabel()
-        lbl_size.setAlignment(Qt.AlignCenter)
-
-        vbox = QVBoxLayout()
-        vbox.addWidget(lbl_img)
-        vbox.addWidget(lbl_size)
-        self.setLayout(vbox)
-
-        btn1 = QPushButton('QUIT', self)
-        btn1.resize(btn1.sizeHint())
-        btn1.clicked.connect(QCoreApplication.instance().quit)
-
-        grid = QGridLayout()
-        self.setLayout(grid)
-
-        grid.addWidget(QLabel('Game:'), 0, 0)
-        grid.addWidget(QLabel('PACMAN'), 0, 1)
-        grid.addWidget(btn1, 0, 2)
-
-        self.setWindowTitle('QGridLayout')
-        self.setGeometry(300, 300, 300, 250)
-        self.show()
-
-    def paintEvent(self, e):
-        qp = QPainter()
-        qp.begin(self)
-        self.draw_point(qp)
-        qp.end()
-
-    def draw_point(self, qp):
-        qp.setPen(QPen(Qt.white, 8))
-        qp.drawPoint(self.width() / 2, self.height() / 2)
+char_to_image = {
+    '.': 'images/dot.png',
+    '=': 'images/wall.png',
+    '*': 'images/power.png',
+}
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyApp()
-    sys.exit(app.exec_())
+
+with open('level-1.txt', 'w') as f:
+    f.write('====================\n'
+            '=................*.=\n'
+            '==========.........=\n'
+            '=....===......======\n'
+            '=..................=\n'
+            '=.......=====......=\n'
+            '====================\n')
+
+def load_level(number):
+    file = "level-%s.txt" % number
+    with open(file) as f:
+        for line in f:
+            row = []
+            for block in line:
+                row.append(block)
+            world.append(row)
+
+def draw():
+    for y, row in enumerate(world):
+        for x, block in enumerate(row):
+            image = char_to_image.get(block, None)
+            if image:
+                img = pygame.image.load(char_to_image[block])
+                screen.blit(img, (x*BLOCK_SIZE, y*BLOCK_SIZE))
+    # pacman = pygame.image.load('images/pacman.png')
+    screen.blit(pacman, (1*BLOCK_SIZE, 1*BLOCK_SIZE))
+
+def on_key_down(key):
+    if key[pygame.K_DOWN]:
+        # pacman.y += BLOCK_SIZE
+        screen.blit(pacman, (1 * BLOCK_SIZE, (1+1) * BLOCK_SIZE))
+load_level(1)
+draw()
+pygame.display.flip()
+for row in world: print(row)
+
+
+
+running = True
+while running:
+    event = pygame.event.wait()
+    if event.type == pygame.QUIT:
+        running = False
+    keys = pygame.key.get_pressed()
+    on_key_down(keys)
+    pygame.display.flip()
+
+pygame.quit()
+
